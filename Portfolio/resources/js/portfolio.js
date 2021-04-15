48c2076d9f9c91611220ebe140438b2434ee3f5c
sunny <sunny@gmail.com>
2021-04-15 18:09:47 +0530

    file added

---
+++ Portfolio/resources/js/portfolio.js
@@ -0,0 +1,12 @@
+// CONSTRUCTORS
+
+// function Person() {
+// 	this.name = 'Brad';
+// }
+
+// // instantiate the person object
+
+// const brad = new Person()
+// console.log(brad);
+
+console.log('Hello')
